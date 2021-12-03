81c2a2522f7f2478046823e9836e05173a0920a2
Erik <000erik001@gmail.com>
2021-12-03 12:46:37 +0400

    Displaying-a-list-of-countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,48 @@
+import axios from "axios";
+
+import React, { useState, useEffect } from "react";
 import { Controls } from "./components/Controls";
 import { Header } from "./components/Header";
+import { List } from "./components/List";
 import { Main } from "./components/Main";
+import { Card } from "./components/Card";
+import { ALL_COUNTRIES } from "./config";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  console.log(countries);
+  useEffect(() => {
+      axios.get(ALL_COUNTRIES).then(({ data }) => setCountries(data));
+  }, []);
   return (
     <>
       <Header />
       <Main>
         <Controls />
+        <List>
+          {countries.map((c) => {
+            const countryInfo = {
+              img: c.flags.png,
+              name: c.name,
+              info: [
+                {
+                  title: "Population",
+                  description: c.population.toLocaleString(),
+                },
+                {
+                  title: "Region",
+                  description: c.region,
+                },
+                {
+                  title: "Capital",
+                  description: c.capital,
+                },
+              ],
+            };
+
+            return <Card key={c.name} {...countryInfo} />;
+          })}
+        </List>
       </Main>
     </>
   );
--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Search } from "./Search";
 import { CustomSelect } from "./CustomSelect";
 import styled from "styled-components";
